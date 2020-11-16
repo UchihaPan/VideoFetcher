60eeaefedf1cdc2ba46628942a314b112b4e4301
panka <pankajshinde798@gmail.com>
2020-11-16 17:57:18 +0530

    finishh

--- videoshandler/admin.py
+++ videoshandler/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import wall,Videos
 
 # Register your models here.
-admin.site.register(wall)
-admin.site.register(Videos)
+
+class WallAdmin(admin.ModelAdmin):
+    list_display=('id','title')
+    list_display_links = ('id','title')
+    list_filter = ('title',)
+    search_fields = ('title',)
+    list_per_page = 25
+
+class VideosAdmin(admin.ModelAdmin):
+    list_display=('id','title','wall','youtube')
+    list_display_links = ('id','title','youtube')
+    list_filter = ('title','youtube')
+    search_fields = ('title','youtube')
+    list_per_page = 25
+
+
+admin.site.register(wall,WallAdmin)
+admin.site.register(Videos,VideosAdmin)
